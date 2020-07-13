827387849d1e4600633f5aa71ada6d93f80c1704
egorkonyagin <egorkonyagin@yandex.ru>
2020-07-13 14:13:21 +0300

    added some updates

--- client_side/client_recorder.py
+++ client_side/client_recorder.py
@@ -1,15 +1,20 @@
 import socket
 import os
 import json
+import struct
 
-HOST = '127.0.0.1'  # The server's hostname or IP address
-PORT = 65432        # The port used by the server
+HOST = '172.16.48.141'   # The server's hostname or IP address
+PORT = 5006        # The port used by the server
 
 def GetStatus():
-	with open("../jsons/status.json") as f:
-		
+	with open("status.json") as f:
+		print("opening status.json..")
 		data = json.load(f)
-	return json.dumps(data)
+	data = json.dumps(data)
+	L = len(data)
+	L_bin = struct.pack('<l', L)
+	print(L, L_bin+data.encode())
+	return L_bin + data.encode()
 
 def AddTask(task):
 	dir_name = "./data/id_" + str(task["TaskID"])
@@ -30,12 +35,17 @@ def Discard(task):
 if __name__ == "__main__":
 	with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 	    s.connect((HOST, PORT))
-	    s.sendall(b'Hello, world')
-	    data = s.recv(1024).decode()
+	    #s.sendall(b'Hello, world')
+	    data = s.recv(1024)
+	    print(data)
+	    l = data[:4]
+	    print(struct.unpack('<l', l), l)
+	    data = data[4:]
+	    print("raw data is: ", data)
 	    command = json.loads(data)
 	    if command["Type"] == "GetStatus":
 	    	print("Got command GetStatus()")
-	    	s.sendall(GetStatus().encode())
+	    	s.sendall(GetStatus())
 	    else:
 	    	print("Unknown command")
 	    data = s.recv(1024).decode()
