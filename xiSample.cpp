ee8ec5d130117d50619b066a43734c25831c9ba6
egorkonyagin <egorkonyagin@yandex.ru>
2020-08-26 16:08:32 +0300

    fixed error with format

--- xiSample.cpp
+++ xiSample.cpp
@@ -12,24 +12,23 @@
 
 #include <fstream>
 
+#include "cos_camera.h"
+
 #define CHANNEL_NUM 1
 
-#include "cos_camera.h"
 
 struct ImwriteArgs{
 	int number;
-	//int format;
 	int height;
 	int width;
 	int cam_nr;
-	std::string format;
 	uint8_t * pixels_corrected;
 };
 
-void* WriteImg(void* args){
+void* WriteImg(void* args, char* format){
 	struct ImwriteArgs * write_args = (struct ImwriteArgs*)args;
 	std::string fname = "image_" + std::to_string(write_args->cam_nr)+\
-			"_"+ std::to_string(write_args->number) + "." + write_args->format;
+			"_"+ std::to_string(write_args->number) + "." + format;
 	const char *filename = fname.c_str();
 	
 	cv::Mat image_to_write(write_args->height, write_args->width,\ 
@@ -38,28 +37,28 @@ void* WriteImg(void* args){
 	return NULL;
 }
 
-void GetParams(int* n_cameras, int* n_images, int* buffer_size, std::string& format){
+void GetParams(int* n_cameras, int* n_images, int* buffer_size, char* format){
 	json cfg;
 	std::ifstream i("cameras_description.json");
 	i >> cfg;
 	*n_cameras =  (int)cfg["n_cameras"];
 	*buffer_size = (int)cfg["buffer_size"];
 	*n_images = (int)cfg["n_images"] / *buffer_size;
-	format = cfg["format"];
+	std::string format_ = cfg["format"];
+	memcpy((void*)format, (void*)format_.c_str(), sizeof(format));
 }
 
 
 int main(int argc, char* argv[])
 {
-	int N_CAMERAS;
-	int N_IMG;
-	int BUFFER_SIZE;
-	std::string format;
+	int N_CAMERAS, N_IMG, BUFFER_SIZE;
+	char format[3];
+	
 	GetParams(&N_CAMERAS, &N_IMG, &BUFFER_SIZE, format);
 	
 	printf("USER_API: n_cameras is %d\n", N_CAMERAS);
 	
-	omp_set_num_threads(BUFFER_SIZE*N_CAMERAS);
+	omp_set_num_threads(BUFFER_SIZE * N_CAMERAS);
 	
 	Camera * cam[N_CAMERAS];
 	uint8_t ** pixels_corrected[N_CAMERAS];
@@ -68,18 +67,16 @@ int main(int argc, char* argv[])
 	struct ImwriteArgs write_args[N_CAMERAS][BUFFER_SIZE];
 	struct ImwriteArgs write_args_old[N_CAMERAS][BUFFER_SIZE];
 
-	int height[N_CAMERAS], width[N_CAMERAS];
-	int img_size[N_CAMERAS];
+	int height[N_CAMERAS], width[N_CAMERAS], img_size[N_CAMERAS];
 	
 	for(int N = 0; N < N_CAMERAS; N++){
 		cam[N] = new Camera(N);
 
-
 		height[N] = cam[N]->GetHeight(), width[N] = cam[N]->GetWidth();
 		img_size[N] = height[N] * width[N] * CHANNEL_NUM * sizeof(uint8_t);
 
-		pixels_corrected[N] = (uint8_t**)malloc(BUFFER_SIZE * sizeof(uint8_t*));
-		pixels_corrected_old[N] = (uint8_t**)malloc(BUFFER_SIZE * sizeof(uint8_t*));
+		pixels_corrected[N] = (uint8_t ** )malloc(BUFFER_SIZE * sizeof(uint8_t*));
+		pixels_corrected_old[N] = (uint8_t ** )malloc(BUFFER_SIZE * sizeof(uint8_t*));
 		
 		for (int i = 0; i < BUFFER_SIZE; i++){
 			pixels_corrected[N][i] = (uint8_t * )malloc(img_size[N]);
@@ -91,12 +88,11 @@ int main(int argc, char* argv[])
 			write_args_old[N][i].height = height[N];
 			write_args_old[N][i].width = width[N];
 			write_args_old[N][i].cam_nr = N;
-			write_args_old[N][i].format = format;
 		}
 		//printf("Allocated successfully!\n");
 		cam[N]->Start();
 	}
-	printf("USER_API: img_format is %s\n", format.c_str());
+	printf("USER_API: img_format is %s\n", format);
 	printf("USER_API: n_frames is %d\n", N_IMG);
 	//Recording part
 	//printf("Starting rec!\n");
@@ -112,7 +108,7 @@ int main(int argc, char* argv[])
 				write_args_old[N][i].pixels_corrected = pixels_corrected_old[N][i];
 			}
 		}
-		//printf("Exited rec loop!\n");
+		printf("Exited rec loop!\n");
 
 		#pragma omp barrier
 		
@@ -127,7 +123,7 @@ int main(int argc, char* argv[])
 					memcpy((void*)&write_args[N][i], (void*)&write_args_old[N][i], sizeof(ImwriteArgs));
 					//printf("Copying-2 OK!\n");
 					write_args[N][i].pixels_corrected = pixels_corrected[N][i];
-					WriteImg((void*)&write_args[N][i]);
+					WriteImg((void*)&write_args[N][i], format);
 				} 
 			}	
 		}		
@@ -142,16 +138,17 @@ int main(int argc, char* argv[])
 		cam[N]->Stop();
 	}
 	
-	printf("USER_API: Time elapsed (png): %f seconds.\n", end-start);
+	printf("USER_API: Time elapsed (%s): %f seconds.\n", format, end-start);
 	
 	for (int N = 0; N < N_CAMERAS; N++){
 		for (int i = 0; i < BUFFER_SIZE; i++){
 			free(pixels_corrected[N][i]);
+			//printf("Freed buffer %d %d\n", N, i);
 		}
 		free(pixels_corrected[N]);
+		//printf("Freed buffer array %d\n", N);
 		delete cam[N];
 	}
-	
 	printf("USER_API: Done!\n");
 	return 0;
 }
